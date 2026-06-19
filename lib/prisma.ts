388f27f8d5be2a3ce9fbc8c7cf9552d0a73ba0a0
meba <mebaww2@gmail.com>
2026-06-19 20:45:30 +0300

    bug fixes

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,22 @@
 import { PrismaClient } from "@/prisma/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DEV_DB_URL!,
+const databaseUrl =
+  process.env.NODE_ENV === "production"
+    ? process.env.DIRECT_URL
+    : process.env.DEV_DB_URL;
+
+if (!databaseUrl) {
+  throw new Error("Database URL is not defined");
+}
+
+const pool = new Pool({
+  connectionString: databaseUrl,
 });
 
-export const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+  adapter,
+});
