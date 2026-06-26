d1517aaa41ae20633d5bf27c8c6cd053f702d8dc
meba <mebaww2@gmail.com>
2026-06-26 12:44:54 +0300

    chore: remove clear_notes.ts utility script

--- clear_notes.ts
+++
@@ -1,6 +0,0 @@
-import { prisma } from './lib/prisma';
-async function main() {
-  const result = await prisma.note.deleteMany();
-  console.log(`Deleted ${result.count} notes.`);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
