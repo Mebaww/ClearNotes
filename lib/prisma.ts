39d46b513c124a6a3cf363f19a03d36f40db46ec
meba <mebaww2@gmail.com>
2026-07-05 18:11:13 +0300

    init

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from "@/prisma/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const databaseUrl = process.env.DB_URL_MAIN;
+const databaseUrl = process.env.DB_URL_DEV
 
 if (!databaseUrl) {
   throw new Error("Database URL is not defined");
